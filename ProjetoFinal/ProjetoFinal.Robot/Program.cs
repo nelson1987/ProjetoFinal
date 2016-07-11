29b40c1b03f75f6d2a76d46faf883fc4fe467e6c
nelson1987 <nelson.ash@outlook.com>
2016-07-11 18:39:41 -0300

    PreCommit

---
+++ ProjetoFinal/ProjetoFinal.Robot/Program.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoFinal.Service.Services;
+using System;
+using System.Collections.Generic;
+using ProjetoFinal.Domain.Entities;
+using ProjetoFinal.Domain.Interfaces.Repository;
+using ProjetoFinal.Domain.Interfaces.Service;
+using ProjetoFinal.Domain.Interfaces.UnitOfWork;
+
+namespace ProjetoFinal.Robot
+{
+    class Program
+    {
+        //private IUsuarioRepository _iUsuarioRepository;
+        public static IUsuarioService _usuarioService;
+        private IUnitOfWork _unitOfWork;
+
+        public Program()
+        {
+            _usuarioService = new UsuarioService(_unitOfWork);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("");
+            Perfil perfilProcurado = new Perfil(1,"Novos");
+            List<Usuario> usuarios = _usuarioService.ListarPorPerfil(perfilProcurado);
+            Console.ReadKey();
+        }
+    }
+}
