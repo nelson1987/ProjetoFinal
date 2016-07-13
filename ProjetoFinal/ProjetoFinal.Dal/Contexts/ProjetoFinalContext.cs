f997402f787313f95fc8429521172ae23b4ccd98
nelson1987 <nelson.ash@outlook.com>
2016-07-12 23:01:57 -0300

    contexto

---
+++ ProjetoFinal/ProjetoFinal.Dal/Contexts/ProjetoFinalContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ProjetoFinal.Domain.Entities;
+
+namespace ProjetoFinal.Dal.Contexts
+{
+    public class ProjetoFinalContext : DbContext
+    {
+        public ProjetoFinalContext()
+            : base("Name=MemberContext")
+        {
+        }
+
+        public DbSet<Usuario> Members { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
+    }
+}
