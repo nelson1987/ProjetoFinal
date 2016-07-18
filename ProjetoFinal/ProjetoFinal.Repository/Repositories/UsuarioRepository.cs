be8c22879643bf7fed54e76b8d00bdb3555786a0
nelson1987 <nelson.ash@outlook.com>
2016-07-17 23:15:10 -0300

    Repository

--- ProjetoFinal/ProjetoFinal.Repository/Repositories/UsuarioRepository.cs
+++ ProjetoFinal/ProjetoFinal.Repository/Repositories/UsuarioRepository.cs
@@ -1,19 +1,22 @@
 ﻿using ProjetoFinal.Domain.Entities;
 using ProjetoFinal.Domain.Interfaces.Repository;
-using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 
 namespace ProjetoFinal.Repository.Repositories
 {
     public class UsuarioRepository : PadraoRepository<Usuario>, IUsuarioRepository
     {
-        public UsuarioRepository(DbContext context) : base(context)
+        public UsuarioRepository(DbContext context)
+            : base(context)
         {
         }
+
         public List<Usuario> ListarPorPerfil(Perfil perfil)
         {
-            throw new NotImplementedException();
+
+            return this.DbSet.Where(c => c.Perfil.Equals(perfil)).ToList();
         }
 
         public override void Insert(Usuario entity)
